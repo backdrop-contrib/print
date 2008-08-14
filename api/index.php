7625e48808ca014c5c7c88d787320411bc95e76b
Joao Ventura <jcnventura-drupal@venturas.org>
2008-08-14 12:00:30 +0000

    Organize everything in folders before the 1.0 release

---
+++ api/index.php
@@ -0,0 +1,12 @@
+<?php
+// $Id$
+
+/** @mainpage Developer Documentation
+ * Welcome to the module developer's documentation. The interesting functions for
+ * themers are those that start with 'theme_'.
+ *
+ * - Components of Drupal
+ *   - @link print.module Printer-friendly version module @endlink
+ *   - @link print_mail.module Send by e-mail module @endlink
+ *   - @link print_pdf.module PDF version module @endlink
+ */
