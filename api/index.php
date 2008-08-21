2e823f7252174e74ab3c67b51a31690573a643ca
Joao Ventura <jcnventura-drupal@venturas.org>
2008-08-21 23:33:45 +0000

    index.php for the API module

--- api/index.php
+++ api/index.php
@@ -6,12 +6,24 @@
  * Homepage of the API reference
  */
 
-/** @mainpage Developer Documentation
+/** @mainpage Printer, e-mail and PDF versions HEAD
  * Welcome to the module developer's documentation. The interesting functions for
  * themers are those that start with 'theme_'.
  *
- * - Components of Drupal
- *   - @link print.module Printer-friendly version module @endlink
- *   - @link print_mail.module Send by e-mail module @endlink
- *   - @link print_pdf.module PDF version module @endlink
+ * - Printer-friendly pages (core)
+ *   - @link print.module Main file @endlink
+ *   - @link print.admin.inc Settings file @endlink
+ *   - @link print.pages.inc Page generation @endlink
+ *   - @link print.install (Un)Install routines @endlink
+ * - Send by e-mail
+ *   - @link print_mail.module Main file @endlink
+ *   - @link print_mail.admin.inc Settings file @endlink
+ *   - @link print_mail.inc Page generation @endlink
+ *   - @link print_mail.install (Un)Install routines @endlink
+ * - PDF version
+ *   - @link print_pdf.module Main file @endlink
+ *   - @link print_pdf.admin.inc Settings file @endlink
+ *   - @link print_pdf.pages.inc Page generation @endlink
+ *   - @link print_pdf.class.inc Auxiliary class @endlink
+ *   - @link print_pdf.install (Un)Install routines @endlink
  */
