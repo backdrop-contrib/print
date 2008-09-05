497941b026b2574bd916922aa920eea5cb02b1a6
Joao Ventura <jcnventura-drupal@venturas.org>
2008-09-05 13:18:03 +0000

    Slightly better API index

--- api/index.php
+++ api/index.php
@@ -11,19 +11,21 @@
  * themers are those that start with 'theme_'.
  *
  * - Printer-friendly pages (core)
- *   - @link print.module Main file @endlink
- *   - @link print.admin.inc Settings file @endlink
- *   - @link print.pages.inc Page generation @endlink
+ *   - @link print.module Module main file @endlink
+ *   - @link print.admin.inc Settings form @endlink
+ *   - @link print.pages.inc HTML generation @endlink
  *   - @link print.install (Un)Install routines @endlink
+ *   - @link print.tpl.php Page generation template @endlink
  * - Send by e-mail
- *   - @link print_mail.module Main file @endlink
- *   - @link print_mail.admin.inc Settings file @endlink
- *   - @link print_mail.inc Page generation @endlink
+ *   - @link print_mail.module Module main file @endlink
+ *   - @link print_mail.admin.inc Settings form @endlink
+ *   - @link print_mail.inc Mail form and send mail routine @endlink
  *   - @link print_mail.install (Un)Install routines @endlink
  * - PDF version
- *   - @link print_pdf.module Main file @endlink
- *   - @link print_pdf.admin.inc Settings file @endlink
- *   - @link print_pdf.pages.inc Page generation @endlink
- *   - @link print_pdf.class.inc Auxiliary class @endlink
+ *   - @link print_pdf.module Module main file @endlink
+ *   - @link print_pdf.admin.inc Settings form @endlink
+ *   - @link print_pdf.pages.inc PDF generation @endlink
+ *   - @link print_pdf.class.inc Auxiliary PHP5 class @endlink
+ *   - @link print_pdf.class_php4.inc Auxiliary PHP4 class @endlink
  *   - @link print_pdf.install (Un)Install routines @endlink
  */
