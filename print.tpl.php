4f1c1fc7c440aea0d151a54f8fa807e992039589
Joao Ventura <jcnventura-drupal@venturas.org>
2009-05-13 23:13:07 +0000

    Move title after the meta content-type tag to prevent XSS attacks

--- print.tpl.php
+++ print.tpl.php
@@ -9,8 +9,8 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" lang="<?php print $print['language']; ?>" xml:lang="<?php print $print['language']; ?>">
   <head>
-    <title><?php print $print['title']; ?></title>
     <?php print $print['head']; ?>
+    <title><?php print $print['title']; ?></title>
     <?php print $print['scripts']; ?>
     <?php print $print['robots_meta']; ?>
     <?php print $print['base_href']; ?>
