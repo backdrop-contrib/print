4928335f6a9b43a29d6c24c921ec2d0cbcfd126f
Joao Ventura <jcnventura-drupal@venturas.org>
2009-05-21 19:09:59 +0000

    Fix #469458: Web version passes XML 1.0 Strict validation test

--- print.tpl.php
+++ print.tpl.php
@@ -19,11 +19,11 @@
   </head>
   <body<?php print $print['sendtoprinter']; ?>>
     <?php if (!empty($print['message'])) {
-      print '<div class="print-message">'. $print['message'] .'</div><br />';
+      print '<div class="print-message">'. $print['message'] .'</div><p />';
     } ?>
     <div class="print-logo"><?php print $print['logo']; ?></div>
     <div class="print-site_name"><?php print $print['site_name']; ?></div>
-    <br />
+    <p />
     <div class="print-breadcrumb"><?php print $print['breadcrumb']; ?></div>
     <hr class="print-hr" />
     <h1 class="print-title"><?php print $print['title']; ?></h1>
