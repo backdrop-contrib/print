d08475b8b7ceefb6ad23562e0d87ec4bf61a49bb
Joao Ventura <jcnventura-drupal@venturas.org>
2009-07-09 12:01:51 +0000

    Stop using @mainpage in API documentation comments

--- print.tpl.php
+++ print.tpl.php
@@ -4,6 +4,8 @@
 /**
  * @file
  * Default print module template
+ *
+ * @ingroup print
  */
 ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
