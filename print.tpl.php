2096205a8d9dd0df3c8fb3f39f9cf9509e15ecd8
Joao Ventura <jcnventura-drupal@venturas.org>
2010-08-18 00:33:40 +0000

    Fix #883952: Put base href at the top of the print template

--- print.tpl.php
+++ print.tpl.php
@@ -12,11 +12,11 @@
 <html xmlns="http://www.w3.org/1999/xhtml" lang="<?php print $print['language']; ?>" xml:lang="<?php print $print['language']; ?>">
   <head>
     <?php print $print['head']; ?>
+    <?php print $print['base_href']; ?>
     <title><?php print $print['title']; ?></title>
     <?php print $print['scripts']; ?>
     <?php print $print['sendtoprinter']; ?>
     <?php print $print['robots_meta']; ?>
-    <?php print $print['base_href']; ?>
     <?php print $print['favicon']; ?>
     <?php print $print['css']; ?>
   </head>
