a779e64d1b2a2757ed7f3df3bac0f5de564b04b1
Joao Ventura <jcnventura-drupal@venturas.org>
2012-05-14 00:13:14 +0200

    Issue #953920: Stop using the print array created by_print_var_generator(). Instead make the print.tpl.php a real template and move most of the work into print_preprocess_print().

--- print.tpl.php
+++ print.tpl.php
@@ -7,32 +7,41 @@
  * @ingroup print
  */
 ?>
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml" lang="<?php print $print['language']; ?>" xml:lang="<?php print $print['language']; ?>">
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML+RDFa 1.0//EN"
+  "http://www.w3.org/MarkUp/DTD/xhtml-rdfa-1.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="<?php print $language->language; ?>" version="XHTML+RDFa 1.0" dir="<?php print $language->dir; ?>">
   <head>
-    <?php print $print['head']; ?>
-    <?php print $print['base_href']; ?>
-    <title><?php print $print['title']; ?></title>
-    <?php print $print['scripts']; ?>
-    <?php print $print['sendtoprinter']; ?>
-    <?php print $print['robots_meta']; ?>
-    <?php print $print['favicon']; ?>
-    <?php print $print['css']; ?>
+    <?php print $head; ?>
+    <base href='<?php print $url ?>' />
+    <title><?php print $print_title; ?></title>
+    <?php print $scripts; ?>
+    <?php if (isset($sendtoprinter)) print $sendtoprinter; ?>
+    <?php print $robots_meta; ?>
+    <?php if (theme_get_setting('toggle_favicon')): ?>
+      <link rel='shortcut icon' href='<?php print theme_get_setting('favicon') ?>' type='image/x-icon' />
+    <?php endif; ?>
+    <?php print $css; ?>
   </head>
   <body>
-    <?php if (!empty($print['message'])) {
-      print '<div class="print-message">'. $print['message'] .'</div><p />';
-    } ?>
-    <div class="print-logo"><?php print $print['logo']; ?></div>
-    <div class="print-site_name"><?php print $print['site_name']; ?></div>
+    <?php if (!empty($message)): ?>
+      <div class="print-message"><?php print $message; ?></div><p />
+    <?php endif; ?>
+    <?php if ($print_logo): ?>
+      <div class="print-logo"><?php print $print_logo; ?></div>
+    <?php endif; ?>
+    <div class="print-site_name"><?php print theme('print_published'); ?></div>
     <p />
-    <div class="print-breadcrumb"><?php print $print['breadcrumb']; ?></div>
+    <div class="print-breadcrumb"><?php print theme('print_breadcrumb', array('node' => $node)); ?></div>
     <hr class="print-hr" />
-    <div class="print-content"><?php print $print['content']; ?></div>
-    <div class="print-footer"><?php print $print['footer_message']; ?></div>
+    <div class="print-content"><?php print $content; ?></div>
+    <div class="print-footer"><?php print theme('print_footer'); ?></div>
     <hr class="print-hr" />
-    <div class="print-source_url"><?php print $print['source_url']; ?></div>
-    <div class="print-links"><?php print $print['pfp_links']; ?></div>
-    <?php print $print['footer_scripts']; ?>
+    <?php if ($sourceurl_enabled): ?>
+      <div class="print-source_url">
+        <?php print theme('print_sourceurl', array('url' => $source_url, 'node' => $node, 'cid' => $cid)); ?>
+      </div>
+    <?php endif; ?>
+    <div class="print-links"><?php print theme('print_url_list'); ?></div>
+    <?php print $footer_scripts; ?>
   </body>
 </html>
