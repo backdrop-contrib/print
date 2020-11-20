f778b1618e4c0a0629d15a1959d9cf07a36253d1
laryn <laryn@cedc.org>
2020-11-20 14:49:55 -0600

    WIP cleanup port

--- print.tpl.php
+++ print.tpl.php
@@ -66,11 +66,12 @@
  * @see page.tpl.php
  * @ingroup print
  */
+ 
 ?>
 
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML+RDFa 1.0//EN"
   "http://www.w3.org/MarkUp/DTD/xhtml-rdfa-1.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="<?php print $language->language; ?>" version="XHTML+RDFa 1.0" dir="<?php print $language->dir; ?>">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="<?php print $html_attributes['lang']; ?>" version="XHTML+RDFa 1.0" dir="<?php $html_attributes['dir']; ?>">
   <head>
     <?php print $head; ?>
     <base href='<?php print $url ?>' />
