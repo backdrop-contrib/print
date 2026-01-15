264cbf11093a2c7ea8b05e553321985faea81710
Jen Lampton <jen@jenerationweb.com>
2026-01-14 18:46:11 -0800

    Issue #39: Move print pdf settings into the vertical tab on the node type form.

---
+++ print_pdf/js/print_pdf.js
@@ -0,0 +1,32 @@
+/**
+ * @file
+ * Sets the summary for Content moderation on vertical tabs.
+ */
+
+(function ($) {
+
+Backdrop.behaviors.printPdfSettingsSummary = {
+  attach: function(context) {
+    var $context = $(context);
+    $context.find('fieldset#edit-print-pdf').backdropSetSummary(function () {
+      var vals = [];
+
+      console.log($('select[name="print_pdf_size"]', context).val());
+
+      if ($('select[name="print_pdf_size"]', context).val()) {
+        vals.push('Size ' + Backdrop.checkPlain($('select[name="print_pdf_size"] option:selected').text()));
+      }
+      else {
+        vals.push(Backdrop.t('Size unchanged'));
+      }
+
+      if ($('select[name="print_pdf_orientation"]', context).val()) {
+        vals.push(Backdrop.checkPlain($('select[name="print_pdf_orientation"] option:selected').text()));
+      }
+
+      return vals.join(', ');
+    });
+  }
+};
+
+})(jQuery);
